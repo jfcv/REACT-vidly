2993d2606c48b9707099f3acdb5edbd459aa143d
Juan Cuadros <juan.cuadros@pm.me>
2022-01-21 19:25:07 -0500

    [Route Parameters. Query String. Redirect. Not Found Pages. Nested Routing. Components added: Customers, NavBar, Rentals, NotFound, MovieForm.]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import React from "react";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Navigate to="/movies" />} />
+          <Route path="/movies/:id" element={<MovieForm />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/rentals" element={<Rentals />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
