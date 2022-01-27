f74fb378d28f4ed16181245794d939041437006f
Juan Cuadros <juan.cuadros@pm.me>
2022-01-27 12:20:33 -0500

    Validating with Joi. Extracting a reusable Form component. Usint the ...rest operator for processing the parameters at the Input component. Extracting Helper rendering methods. Form Component implemented.

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
 
-const Input = ({ name, label, value, onChange, type, error }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type={type}
-        className="form-control"
-      />
+      <input {...rest} id={name} name={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,90 +1,31 @@
+import Form from "./common/form";
 import React, { Component } from "react";
-import Input from "./common/input";
+const Joi = require("joi-browser");
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
-  /**
-   * creating a ref to access to the input field
-   * and the using to autofocus the input field
-   */
-  /*
-  username = React.createRef();
-
-  componentDidMount() {
-    this.username.current.focus();
-  }
-  */
-
-  validate = () => {
-    const errors = {};
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
-  validateProperty = ({ name, value }) => {
-    if (name === "username")
-      if (value.trim() === "") return "Username is required.";
-    if (name === "password")
-      if (value.trim() === "") return "Password is required.";
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     // Call the sever
-    console.log("submitted");
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
+    console.log("Submitted");
   };
 
   render() {
-    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            label="Username"
-            value={account.username}
-            onChange={this.handleChange}
-            type="text"
-            error={errors.username}
-          />
-
-          <Input
-            name="password"
-            label="Password"
-            value={account.password}
-            onChange={this.handleChange}
-            type="password"
-            error={errors.password}
-          />
-
-          <button className="btn btn-primary">Login</button>
+          {this.renderInput("username")}
+          {this.renderInput("password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
